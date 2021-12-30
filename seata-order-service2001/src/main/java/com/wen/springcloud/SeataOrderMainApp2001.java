f36874ceb7fd0093e69101bd472eac9a94f69a1e
wangwenge <18500035373@163.com>
2021-12-30 11:20:05 +0800

    spring boot + spring cloud 微服务架构学习

--- seata-order-service2001/src/main/java/com/wen/springcloud/SeataOrderMainApp2001.java
+++ seata-order-service2001/src/main/java/com/wen/springcloud/SeataOrderMainApp2001.java
@@ -14,6 +14,12 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 public class SeataOrderMainApp2001 {
     /**
      * 启动服务之前先启动nacos、再启动seata TC 端 window命令：seata-server.bat -p 8091 -h 127.0.0.1 -m db
+     * 使用nacos作为配置中心需要将config.txt（包括server端和client端）配置文件上送到nacos 使用以下命令
+     * sh nacos-config.sh -h 47.102.97.183 -p 8848 -g DEFAULT_GROUP -t e039ecd9-d1bb-490b-bc32-e8a5459bd89c -u nacos -w nacos
+     * -h -p 指定nacos的端口地址；
+     * -g 指定配置的分组，注意，是配置的分组；
+     * -t 指定命名空间id；
+     * -u -w指定nacos的用户名和密码，开启了nacos注册和配置认证的才需要指定。
      * @param args
      */
     public static void main(String[] args) {
